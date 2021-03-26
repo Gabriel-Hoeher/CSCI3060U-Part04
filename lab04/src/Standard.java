622bfd14f6ff7ce8ca106484ab38d66fb159d52c
Will-Dolan-VI <78272577+Will-Dolan-VI@users.noreply.github.com>
2021-03-25 21:43:19 -0400

    Added Standard

---
+++ lab04/src/Standard.java
@@ -0,0 +1,21 @@
+public class Standard extends Transaction{
+  void withdraw(){
+    // Take out some money
+
+  }
+  
+  void transfer(){
+    // move money from one to another
+
+  }
+  
+  void paybill(){
+    // Would this work the same as withdraw?
+
+  }
+  
+  void deposit(){
+    // Add money
+  }
+  
+}
